6ad0c434d53803c1679f52c301ef4e3d9cca5f4e
abhay-sen <abhaysen0502@gmail.com>
2024-03-01 12:10:07 +0530

    finally

---
+++ CONFIG.js
@@ -0,0 +1,10 @@
+const CONFIGDATA = {
+  titleHeader: "Jaan I know you are in a lot of pain right now I know this wont make any difference but might let you feel a bit better ❤️",
+  descriptionHeader:
+    "Jaan you are the girl of my dreams and i really love you do you love me??",
+  buttonYes: "No, I HATE YOU",
+  buttonNo: "Yes, I love you",
+  titleModar: "😩 I know you hate me!! Poor AB:)",
+  descriptionModar:
+    "Hehe kaisa laga mera mazaak, Jaan i really know how much you love me.... </br> And i know that you love me more than anyone else existing to be very honest i'm greatfull for your love bohut jyada!! </br> And trust me when i say i am here to stay i am going to stay i really love you meri jaan....</br> I am here for you jaan.. <3",
+};
